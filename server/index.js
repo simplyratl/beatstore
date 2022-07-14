c8c0d704b75e055b9b430720f4c78b86fd1f3a5c
simplyratl <nikicaraznatovic123@gmail.com>
2022-07-14 23:10:43 +0200

    Preparing for Deployment

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use("/transaction", transactionRoute);
 
 //Serve static assets if in production
 // app.use(express.static(path.join(__dirname, "client/build")));
-app.use(express.static("client/build"));
+app.use(express.static("app/client/build"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
